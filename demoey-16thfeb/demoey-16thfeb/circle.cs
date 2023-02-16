1c49c82012b7697ded8cc9b0e6cd7875ef4e8e7a
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-16 11:39:31 +0000

    ifelse and switch program

--- demoey-16thfeb/demoey-16thfeb/circle.cs
+++ demoey-16thfeb/demoey-16thfeb/circle.cs
@@ -1,101 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace demoey_16thfeb
-{
-	class circle
-	{
-		static void Main(string[] args)
-		{
+//namespace demoey_16thfeb
+//{
+//	class circle
+//	{
+//		static void Main(string[] args)
+//		{
 
-			int a1 = 10;
-			float a2 = 10.23423f;
-			char ch = 'a';
-			string s1 = "abc";
+//			int a1 = 10;
+//			float a2 = 10.23423f;
+//			char ch = 'a';
+//			string s1 = "abc";
 
-			Console.WriteLine(a1);
-			Console.WriteLine(a2);
-			Console.WriteLine(ch);
-			Console.WriteLine(s1);
-
-
-
-			/* practice program 1 */
-
-			Console.WriteLine("***************************");
-
-			Console.Write("Enter your name : ");
-			string name = Console.ReadLine();
-
-			Console.Write("Enter your Roll No. : ");
-			int roll = int.Parse(Console.ReadLine());
-
-			Console.WriteLine("Your name is " + name);
-			Console.WriteLine("Your Roll no. is " + roll);
-
-			/* practice program 2 */
-
-			Console.WriteLine("***************************");
-
-			Console.Write("Enter 1st num : ");
-			int num1 = int.Parse(Console.ReadLine());
-
-			Console.Write("Enter 2nd num : ");
-			int num2 = int.Parse(Console.ReadLine());
-
-			Console.WriteLine("Your results are");
-			Console.WriteLine("Addition : " + (num1 + num2));
-			Console.WriteLine("Multiplication : " + (num1 * num2));
-			Console.WriteLine("Subtraction : " + (num1 - num2));
-			Console.WriteLine("Division : " + (num1 / num2));
-
-			/*Practice program 3*/
-
-			Console.WriteLine("***************************");
-
-			Console.Write("Enter principle amount : ");
-			int p = int.Parse(Console.ReadLine());
-
-			Console.Write("Enter interest : ");
-			float i = float.Parse(Console.ReadLine());
-
-			Console.Write("Enter time in years : ");
-			int t = int.Parse(Console.ReadLine());
-
-			float si = (p * i * t) / 100;
-
-			Console.WriteLine("Your SI is : " + si);
-
-			/*Practice program 4*/
-
-			Console.WriteLine("***************************");
+//			Console.WriteLine(a1);
+//			Console.WriteLine(a2);
+//			Console.WriteLine(ch);
+//			Console.WriteLine(s1);
 
 
 
+//			/* practice program 1 */
 
-			const float pi = 3.142f;
+//			Console.WriteLine("***************************");
 
-			Console.Write("Enter the radius : ");
-			float r = float.Parse(Console.ReadLine());
+//			Console.Write("Enter your name : ");
+//			string name = Console.ReadLine();
+
+//			Console.Write("Enter your Roll No. : ");
+//			int roll = int.Parse(Console.ReadLine());
+
+//			Console.WriteLine("Your name is " + name);
+//			Console.WriteLine("Your Roll no. is " + roll);
+
+//			/* practice program 2 */
+
+//			Console.WriteLine("***************************");
+
+//			Console.Write("Enter 1st num : ");
+//			int num1 = int.Parse(Console.ReadLine());
+
+//			Console.Write("Enter 2nd num : ");
+//			int num2 = int.Parse(Console.ReadLine());
+
+//			Console.WriteLine("Your results are");
+//			Console.WriteLine("Addition : " + (num1 + num2));
+//			Console.WriteLine("Multiplication : " + (num1 * num2));
+//			Console.WriteLine("Subtraction : " + (num1 - num2));
+//			Console.WriteLine("Division : " + (num1 / num2));
+
+//			/*Practice program 3*/
+
+//			Console.WriteLine("***************************");
+
+//			Console.Write("Enter principle amount : ");
+//			int p = int.Parse(Console.ReadLine());
+
+//			Console.Write("Enter interest : ");
+//			float i = float.Parse(Console.ReadLine());
+
+//			Console.Write("Enter time in years : ");
+//			int t = int.Parse(Console.ReadLine());
+
+//			float si = (p * i * t) / 100;
+
+//			Console.WriteLine("Your SI is : " + si);
+
+//			/*Practice program 4*/
+
+//			Console.WriteLine("***************************");
 
 
-			Console.WriteLine("The area is : " + (pi * r * r));
-			Console.WriteLine("The circumference is : " + (2 * pi * r));
+
+
+//			const float pi = 3.142f;
+
+//			Console.Write("Enter the radius : ");
+//			float r = float.Parse(Console.ReadLine());
+
+
+//			Console.WriteLine("The area is : " + (pi * r * r));
+//			Console.WriteLine("The circumference is : " + (2 * pi * r));
 
 			
 
-			byte[] array1 = { 2, 4, 6, 8, 10 };
-			Console.WriteLine("BArray is :{0}", BitConverter.ToString(array1));
+//			byte[] array1 = { 2, 4, 6, 8, 10 };
+//			Console.WriteLine("BArray is :{0}", BitConverter.ToString(array1));
 
-			Console.WriteLine();
-			String str = Convert.ToBase64String(array1);
-			Console.WriteLine("Base 64 string is :{0}", str);
+//			Console.WriteLine();
+//			String str = Convert.ToBase64String(array1);
+//			Console.WriteLine("Base 64 string is :{0}", str);
 
-			Console.ReadKey(); 
-			//update
-		}
-	}
-}
+//			Console.ReadKey(); 
+//			//update
+//		}
+//	}
+//}
---
+++ demoey-16thfeb/demoey-16thfeb/ifelseprgm.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demoey_16thfeb
+{
+	class ifelseprgm
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter month name in short");
+			string month = Console.ReadLine();
+
+			if (month == "january")
+				Console.WriteLine(1);
+
+			if (month == "feburary")
+				Console.WriteLine(2);
+
+			if (month == "march")
+				Console.WriteLine(3);
+
+			if (month == "april")
+				Console.WriteLine(4);
+
+			if (month == "may")
+				Console.WriteLine(5);
+
+			if (month == "june")
+				Console.WriteLine(6);
+
+			if (month == "july")
+				Console.WriteLine(7);
+
+			if (month == "august")
+				Console.WriteLine(8);
+
+			if (month == "september")
+				Console.WriteLine(9);
+
+			if (month == "october")
+				Console.WriteLine(10);
+
+			if (month == "novemebr")
+				Console.WriteLine(11);
+
+			if (month == "december")
+				Console.WriteLine(12);
+
+
+			//program 2
+
+
+			Console.WriteLine("enter age");
+			int age = int.Parse(Console.ReadLine());
+
+			if (age >= 18)
+				Console.WriteLine("Eligible to vote");
+			else
+				Console.WriteLine("Not eligible to vote");
+
+
+			//program 3 
+			Console.WriteLine("Enter day name");
+			string day = Console.ReadLine();
+
+			switch (day)
+			{
+				case "monday":
+					Console.WriteLine(1);
+					break;
+
+				case "tuesday":
+					Console.WriteLine(2);
+					break;
+
+				case "wednesday":
+					Console.WriteLine(3);
+					break;
+
+				case "thrusday":
+					Console.WriteLine(4);
+					break;
+
+				case "friday":
+					Console.WriteLine(5);
+					break;
+
+				case "saturday":
+					Console.WriteLine(6);
+					break;
+
+				case "sunday":
+					Console.WriteLine(7);
+					break;
+			}
+			//program 4
+
+			Console.WriteLine(" Enter num1");
+			int num1 = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter num1");
+			int num2 = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter symbol");
+			string symbol = Console.ReadLine();
+
+			switch (symbol)
+			{
+				case "+":
+					Console.WriteLine(num1 + num2);
+					break;
+
+				case "-":
+					Console.WriteLine(num1 - num2);
+					break;
+
+				case "*":
+					Console.WriteLine(num1 * num2);
+					break;
+
+				case "/":
+					Console.WriteLine(num1 / num2);
+					break;
+			}
+
+			Console.ReadKey();
+		}
+	}
+
+
+}
