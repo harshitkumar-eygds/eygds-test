d567663ad8680ebc0ddf3af1507dcae774903bab
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-17 04:15:02 +0000

    17th feb program initial commit

---
+++ eydemo17thfeb/eydemo17thfeb/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eydemo17thfeb
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			char[] arr = { 'a', 'e', 'i', 'o', 'u' };
+
+			String s1 = new string(arr);
+
+			Console.WriteLine(s1);
+
+			string s2 = "write in c#";
+
+			Console.WriteLine(s2);
+
+			string s3 = "data";
+			Console.WriteLine(s3);
+
+			Console.ReadKey();
+
+		}
+	}
+}
