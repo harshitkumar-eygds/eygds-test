f9ea0473afa3df48049a01a22585accd0a6a5b19
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-21 09:50:05 +0000

    3d array

---
+++ eydemo17thfeb/eydemo17thfeb/3darray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eydemo17thfeb
+{
+    class array3d
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter row : ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Enter Col : ");
+            int col = int.Parse(Console.ReadLine());
+            int[,] arr = new int[row, col];
+            
+            Console.WriteLine($"Enter {row * col} numbers");
+
+            for (int i = 0; i < row; i++) 
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    arr[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Your array is");
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(arr[i, j] + " ");
+                    
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- eydemo17thfeb/eydemo17thfeb/andor.cs
+++ eydemo17thfeb/eydemo17thfeb/andor.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace eydemo17thfeb
-{
-    class andor
-    {
-        static void Main(string[] args)
-        {
-            Console.Write("Enter a number : ");
-            int num = int.Parse(Console.ReadLine());
-            int fact = 1;
-            for (int i = 1; i <= num; i++) 
-            {
-                fact = fact * i;    
-            }
-            Console.WriteLine("factorial is : " + fact);
-
-
+//namespace eydemo17thfeb
+//{
+//    class andor
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.Write("Enter a number : ");
+//            int num = int.Parse(Console.ReadLine());
+//            int fact = 1;
+//            for (int i = 1; i <= num; i++) 
+//            {
+//                fact = fact * i;    
+//            }
+//            Console.WriteLine("factorial is : " + fact);
 
 
-            Console.Write("Enter a number : ");
-            string str = Console.ReadLine();
-            bool ispalindrome = true;
 
-            for (int i = 0; i <= str.Length/2; i++) 
-            {
-                if (str[i] == str[str.Length - 1 - i])
-                    ispalindrome = true;
-                else
-                    ispalindrome = false;
-            }
-            if(ispalindrome)
-                   Console.WriteLine("The given string is Palindrome ");
-            else
-                Console.WriteLine("The given string is not a Palindrome ");
 
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.Write("Enter a number : ");
+//            string str = Console.ReadLine();
+//            bool ispalindrome = true;
+
+//            for (int i = 0; i <= str.Length/2; i++) 
+//            {
+//                if (str[i] == str[str.Length - 1 - i])
+//                    ispalindrome = true;
+//                else
+//                    ispalindrome = false;
+//            }
+//            if(ispalindrome)
+//                   Console.WriteLine("The given string is Palindrome ");
+//            else
+//                Console.WriteLine("The given string is not a Palindrome ");
+
+//            Console.ReadKey();
+//        }
+//    }
+//}
