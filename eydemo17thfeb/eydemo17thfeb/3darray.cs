1cddfa372d776e67035318c3ae45a7b51c025889
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-21 10:28:05 +0000

    matrix addition

--- eydemo17thfeb/eydemo17thfeb/3darray.cs
+++ eydemo17thfeb/eydemo17thfeb/3darray.cs
@@ -10,6 +10,7 @@ namespace eydemo17thfeb
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter details of matrix 1");
             Console.Write("Enter row : ");
             int row = int.Parse(Console.ReadLine());
             Console.Write("Enter Col : ");
@@ -37,6 +38,52 @@ namespace eydemo17thfeb
                 }
                 Console.WriteLine();
             }
+
+
+            Console.WriteLine("Enter details of matrix 2");
+            Console.Write("Enter row : ");
+            int row1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Col : ");
+            int col1 = int.Parse(Console.ReadLine());
+            int[,] arr1 = new int[row1, col1];
+
+            Console.WriteLine($"Enter {row1 * col1} numbers");
+
+            for (int i = 0; i < row1; i++)
+            {
+                for (int j = 0; j < col1; j++)
+                {
+                    arr1[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Your array is");
+
+            for (int i = 0; i < row1; i++)
+            {
+                for (int j = 0; j < col1; j++)
+                {
+                    Console.Write(arr1[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+
+            if (row == row1 && col == col1)
+            {
+                Console.WriteLine("matrix addition can be performed");
+
+                for (int i = 0; i < row; i++) {
+                    for (int j = 0; j < col; j++) {
+                        Console.Write(arr[i, j] + arr1[i,j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else {
+                Console.WriteLine("matrix addition cannot be performed");
+            }
+            
             Console.ReadKey();
         }
     }
