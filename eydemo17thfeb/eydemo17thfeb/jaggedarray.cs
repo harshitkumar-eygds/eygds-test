b887f31121f8f0f4304b9947667aa3e2c8ffad4f
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-23 07:15:47 +0000

    update linkedlist

--- eydemo17thfeb/eydemo17thfeb/jaggedarray.cs
+++ eydemo17thfeb/eydemo17thfeb/jaggedarray.cs
@@ -1,31 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace eydemo17thfeb
-{
-    class jaggedarray
-    {
-        static void Main(string[] args)
-        {
-            int[][] arr = new int[4][];
-            arr[0] = new int[] { 1, 2, 3,4 };
-            arr[1] = new int[] { 2, 5, 8 };
-            arr[2] = new int[] { 1, 2, 3, 4, 4,5 };
-            arr[3] = new int[] { 2, 5, 8,2,12,4 }; 
+//namespace eydemo17thfeb
+//{
+//    class jaggedarray
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[][] arr = new int[4][];
+//            arr[0] = new int[] { 1, 2, 3,4 };
+//            arr[1] = new int[] { 2, 5, 8 };
+//            arr[2] = new int[] { 1, 2, 3, 4, 4,5 };
+//            arr[3] = new int[] { 2, 5, 8,2,12,4 }; 
 
-            for (int i = 0; i < arr.Length; i++) 
-            {
-                for (int j = 0; j < arr[i].Length; j++)
-                {
-                    Console.Write(arr[i][j] + " ");
-                }
-                Console.WriteLine();
-            }
+//            for (int i = 0; i < arr.Length; i++) 
+//            {
+//                for (int j = 0; j < arr[i].Length; j++)
+//                {
+//                    Console.Write(arr[i][j] + " ");
+//                }
+//                Console.WriteLine();
+//            }
 
-            Console.ReadKey();
-        }
-    }
-}
+//            Console.ReadKey();
+//        }
+//    }
+//}
