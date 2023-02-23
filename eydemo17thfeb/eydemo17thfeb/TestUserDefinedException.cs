ef0eb89ae649ae05277fc9ade349852390ad1039
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-23 10:27:28 +0000

    Practice program. Bank balance

---
+++ eydemo17thfeb/eydemo17thfeb/TestUserDefinedException.cs
@@ -0,0 +1,45 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace eydemo17thfeb
+//{
+//    class TestUserDefinedException
+//    {
+//        public class InvalidAgeException : Exception 
+//        {
+//            public InvalidAgeException(string message): base(message) 
+//            { }
+            
+//        }
+//        static void validate(int age) 
+//        {
+//            if (age < 10)
+//            {
+//                throw new InvalidAgeException("Sorry, Please enter the correct age");
+//            }
+//        }
+
+//        public static void Main(string[] args) 
+//        {
+//            try
+//            {
+//                Console.WriteLine("Enter the age");
+//                int age = int.Parse(Console.ReadLine());
+//                validate(age);
+//            }
+//            catch (InvalidAgeException e)
+//            {
+//                Console.WriteLine(e);
+//            }
+
+//            Console.WriteLine("Rest");
+//            Console.ReadKey();
+//        }
+
+//    }
+//}
+
+
