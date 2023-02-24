469b203e3e48bd462ce49016eff19e85bcf29f6e
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-24 10:12:44 +0000

    nested exception

---
+++ eydemo17thfeb/eydemo17thfeb/enumprgm.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace eydemo17thfeb
+//{
+//    class enumprgm
+//    {
+//        enum days { monay, tuesday, wednesday, thrusday, friday, saturday, sunday }
+
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine(days.friday);
+//            Console.WriteLine(days.saturday);
+
+//            int day1 = (int)days.monay;
+//            Console.WriteLine(day1);
+
+
+//            Console.ReadKey();
+
+        
+//        }
+//    }
+//}
---
+++ eydemo17thfeb/eydemo17thfeb/nestedtrycatch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eydemo17thfeb
+{
+    class nestedtrycatch
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter numbers to divide 10");
+            int[] arr1 = new int[5];
+            try
+            {
+                for (int i = 0; i < 5; i++)
+                    arr1[i] = int.Parse(Console.ReadLine());
+                
+                try
+                {
+                    Console.Write("Numbers you have entered is : ");
+                    for (int i = 0; i < 5; i++)
+                        Console.Write( arr1[i] + " ");
+                    
+                    Console.WriteLine();
+
+                    for (int i = 0; i < 5; i++)
+                        Console.WriteLine($"10 divide by {arr1[i]} is : {10 / arr1[i]}");
+                }
+                catch(Exception e) 
+                {
+                    Console.WriteLine("Exception (Inner block) : " + e.Message);
+                }
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Exception (Outer block) : " + e.Message);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
