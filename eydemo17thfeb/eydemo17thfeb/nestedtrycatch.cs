9c5977ba04fac874f35311868d11f32754957d7f
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-24 12:08:03 +0000

    update end day

--- eydemo17thfeb/eydemo17thfeb/nestedtrycatch.cs
+++ eydemo17thfeb/eydemo17thfeb/nestedtrycatch.cs
@@ -1,45 +1,45 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace eydemo17thfeb
-{
-    class nestedtrycatch
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("enter numbers to divide 10");
-            int[] arr1 = new int[5];
-            try
-            {
-                for (int i = 0; i < 5; i++)
-                    arr1[i] = int.Parse(Console.ReadLine());
+//namespace eydemo17thfeb
+//{
+//    class nestedtrycatch
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("enter numbers to divide 10");
+//            int[] arr1 = new int[5];
+//            try
+//            {
+//                for (int i = 0; i < 5; i++)
+//                    arr1[i] = int.Parse(Console.ReadLine());
                 
-                try
-                {
-                    Console.Write("Numbers you have entered is : ");
-                    for (int i = 0; i < 5; i++)
-                        Console.Write( arr1[i] + " ");
+//                try
+//                {
+//                    Console.Write("Numbers you have entered is : ");
+//                    for (int i = 0; i < 5; i++)
+//                        Console.Write( arr1[i] + " ");
                     
-                    Console.WriteLine();
+//                    Console.WriteLine();
 
-                    for (int i = 0; i < 5; i++)
-                        Console.WriteLine($"10 divide by {arr1[i]} is : {10 / arr1[i]}");
-                }
-                catch(Exception e) 
-                {
-                    Console.WriteLine("Exception (Inner block) : " + e.Message);
-                }
-            }
-            catch (Exception e) 
-            {
-                Console.WriteLine("Exception (Outer block) : " + e.Message);
-            }
+//                    for (int i = 0; i < 5; i++)
+//                        Console.WriteLine($"10 divide by {arr1[i]} is : {10 / arr1[i]}");
+//                }
+//                catch(Exception e) 
+//                {
+//                    Console.WriteLine("Exception (Inner block) : " + e.Message);
+//                }
+//            }
+//            catch (Exception e) 
+//            {
+//                Console.WriteLine("Exception (Outer block) : " + e.Message);
+//            }
 
-            Console.ReadKey();
+//            Console.ReadKey();
 
-        }
-    }
-}
+//        }
+//    }
+//}
