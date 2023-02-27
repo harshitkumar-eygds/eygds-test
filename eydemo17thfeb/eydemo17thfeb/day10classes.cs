4cf64f4a852422f0cacca775602c66860e3c9ff4
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-27 07:48:13 +0000

    inheritance update

--- eydemo17thfeb/eydemo17thfeb/day10classes.cs
+++ eydemo17thfeb/eydemo17thfeb/day10classes.cs
@@ -1,32 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace eydemo17thfeb
-{
-    class vehicle
-    {
-        public void enginemaker()
-        {
-            Console.WriteLine("Engine of this vehicle is made in 1989");
+//namespace eydemo17thfeb
+//{
+//    class vehicle
+//    {
+//        public void enginemaker()
+//        {
+//            Console.WriteLine("Engine of this vehicle is made in 1989");
 
-        }
-    }
+//        }
+//    }
     
-    class day10classes
-    {
-        static void Main(string[] args)
-        {
-            vehicle obj = new vehicle();
-            obj.enginemaker();
+//    class day10classes
+//    {
+//        static void Main(string[] args)
+//        {
+//            vehicle obj = new vehicle();
+//            obj.enginemaker();
 
-            scooter obj1 = new scooter();
-            obj1.color();
+//            scooter obj1 = new scooter();
+//            obj1.color();
 
-            Console.ReadKey();
+//            Console.ReadKey();
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ eydemo17thfeb/eydemo17thfeb/inheritanceprgm.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eydemo17thfeb
+{
+    class india 
+    {
+        static private int custid;
+        static private string custname;
+
+        public void display() 
+        {
+            Console.WriteLine("customer id is " + custid);
+            Console.WriteLine("customer name is " + custname);
+        }
+    }
+
+    class uk
+    {
+        protected int empid;
+        protected string empname;
+    }
+
+    class us
+    {
+        public int studid;
+        public string studname;
+    }
+
+    class inheritanceprgm
+    {
+        static void Main(string[] args)
+        {
+            india obj1 = new india();
+            //obj1.xxx;
+            obj1.display();
+
+
+            uk obj2 = new uk();
+            //obj2.xx
+
+            us obj3 = new us();
+            obj3.studid = 3;
+            obj3.studname = "jk";
+
+
+            Console.ReadKey();
+        }
+    }
+}
