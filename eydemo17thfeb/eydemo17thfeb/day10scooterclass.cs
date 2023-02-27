83d80e55cb3ab9817ed5c9e45d4ba2fbd66e590d
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-27 05:04:52 +0000

    day 10 class scooter and vehicle update

---
+++ eydemo17thfeb/eydemo17thfeb/day10scooterclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eydemo17thfeb
+{
+    class scooter
+    {
+        public void color()
+        {
+            Console.WriteLine("Colour of scooter is blue");
+
+        }
+    }
+}
--- eydemo17thfeb/eydemo17thfeb/delegateprgm.cs
+++ eydemo17thfeb/eydemo17thfeb/delegateprgm.cs
@@ -1,52 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace eydemo17thfeb
-{
+//namespace eydemo17thfeb
+//{
     
-    class delegateprgm
-    {
-        public delegate int mydelegate(int a, int b);
-        static void Main(string[] args)
-        {
-            //mydelegate d1 = new mydelegate(add());
-            //d1();
+//    class delegateprgm
+//    {
+//        public delegate int mydelegate(int a, int b);
+//        static void Main(string[] args)
+//        {
+//            //mydelegate d1 = new mydelegate(add());
+//            //d1();
 
 
-            delegateprgm obj1 = new delegateprgm();
-            mydelegate d2 = new mydelegate(obj1.multiply);
-            d2 += divide;
-            d2 += add;
+//            delegateprgm obj1 = new delegateprgm();
+//            mydelegate d2 = new mydelegate(obj1.multiply);
+//            d2 += divide;
+//            d2 += add;
 
-            Console.WriteLine( ); 
+//            Console.WriteLine( ); 
 
-            int a = d2(10, 10);
+//            int a = d2(10, 10);
 
-            Console.WriteLine(a);
-            Console.WriteLine(d2(10, 10)); 
+//            Console.WriteLine(a);
+//            Console.WriteLine(d2(10, 10)); 
 
-            Console.ReadKey();
-        }
+//            Console.ReadKey();
+//        }
 
-        static int add(int a , int b) 
-        {
-            // Console.WriteLine("addition result is : " + (5 + 10)); 
-            return a + b;
-        }
+//        static int add(int a , int b) 
+//        {
+//            // Console.WriteLine("addition result is : " + (5 + 10)); 
+//            return a + b;
+//        }
 
-        public int multiply(int a, int b)
-        {
-            //Console.WriteLine("multiplication result is : " + (5 * 10));
-            return a * b;
-        }
+//        public int multiply(int a, int b)
+//        {
+//            //Console.WriteLine("multiplication result is : " + (5 * 10));
+//            return a * b;
+//        }
 
-        static int divide(int a, int b)
-        {
-            // Console.WriteLine("divide result is : " + (5 / 10));
-            return a / b;
-        }
-    }
-}
+//        static int divide(int a, int b)
+//        {
+//            // Console.WriteLine("divide result is : " + (5 / 10));
+//            return a / b;
+//        }
+//    }
+//}
