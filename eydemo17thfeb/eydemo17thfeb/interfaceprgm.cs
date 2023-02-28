17581bd632b5ed0a98f4b43783d99d8665ace3d8
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-02-28 05:18:47 +0000

    copy constructor

--- eydemo17thfeb/eydemo17thfeb/interfaceprgm.cs
+++ eydemo17thfeb/eydemo17thfeb/interfaceprgm.cs
@@ -1,48 +1,112 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading.Tasks;
 
-namespace eydemo17thfeb
-{
-    interface Imyinterface 
-    {
-        void add();
-        void remove();
-    }
 
-    interface Inewinterface
-    {
-        void clear();
-    }
+////// implicit way
+////namespace eydemo17thfeb
+////{
+////    interface Imyinterface 
+////    {
+////        void add();
+////        void remove();
+////    }
 
-    class myclass : Imyinterface, Inewinterface   //implementation
-    {
-        public void add()
-        {
-            Console.WriteLine("this is add function of Imyinterface");
-        }
+////    interface Inewinterface
+////    {
+////        void clear();
+////    }
 
-        public void clear()
-        {
-            Console.WriteLine("this is clear function of Inewinterface");
-        }
+////    class myclass : Imyinterface, Inewinterface   //implementation
+////    {
+////        public void add()
+////        {
+////            Console.WriteLine("this is add function of Imyinterface");
+////        }
 
-        public void remove()
-        {
-            Console.WriteLine("this is remove function of Imyinterface");
-        }
-    }
+////        public void clear()
+////        {
+////            Console.WriteLine("this is clear function of Inewinterface");
+////        }
 
-    class interfaceprgm 
-    {
-        static void Main(string[] args)
-        {
-            myclass obj1 = new myclass();
-            obj1.add();
-            obj1.clear();
-            obj1.remove();
-        }
-    }
-}
+////        public void remove()
+////        {
+////            Console.WriteLine("this is remove function of Imyinterface");
+////        }
+////    }
+
+////    class interfaceprgm 
+////    {
+////        static void Main(string[] args)
+////        {
+////            myclass obj1 = new myclass();
+////            obj1.add();
+////            obj1.clear();
+////            obj1.remove();
+////        }
+////    }
+////}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+////explicit
+
+//namespace eydemo17thfeb
+//{
+//    interface Imyinterface
+//    {
+//        void add();
+//        void remove();
+//    }
+
+//    interface Inewinterface
+//    {
+//        void clear();
+//    }
+
+//    class myclass : Imyinterface, Inewinterface   //implementation
+//    {
+//        void Imyinterface.add()
+//        {
+//            Console.WriteLine("this is add function of Imyinterface(explicit)");
+//        }
+
+//        void Inewinterface.clear()
+//        {
+//            Console.WriteLine("this is clear function of Inewinterface(explicit)");
+//        }
+
+//        void Imyinterface.remove()
+//        {
+//            Console.WriteLine("this is remove function of Imyinterface(explicit)");
+//        }
+//    }
+
+//    class interfaceprgm
+//    {
+//        static void Main(string[] args)
+//        {
+//            myclass obj1 = new myclass();  //cannot invoke explicit interfaces
+//            //cannot create object of interface
+
+//            //make object of interface and initiate with class
+//            Imyinterface m2 = new myclass();
+//            m2.add();
+//            m2.remove();
+
+//            Inewinterface m3 = new myclass();
+//            m3.clear();
+
+//            //obj1.add();
+//            //obj1.clear();
+//            //obj1.remove();
+//        }
+//    }
+//}
---
+++ eydemo17thfeb/eydemo17thfeb/sealedprgm.cs
@@ -0,0 +1,43 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace eydemo17thfeb
+//{
+//    sealed class A // stop the inheritance
+//    {
+//        public int a;
+
+//        public void update(int x) 
+//        {
+//            a = x;
+//        }
+//        public void display() 
+//        {
+//            Console.WriteLine("a is " + a);
+//        }
+//    }
+
+//    //error -- cannot inherit anything
+//    //class B : A
+//    //{
+
+//    //}
+
+
+//    class sealedprgm
+//    {
+//        static void Main(string[] args)
+//        {
+//            A obj1 = new A();
+//            obj1.update(3);
+//            obj1.display();
+
+//            Console.ReadKey();
+//        }
+        
+
+//    }
+//}
