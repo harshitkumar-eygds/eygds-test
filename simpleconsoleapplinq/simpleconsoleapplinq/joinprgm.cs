29fecbbc54e70030168618e0cf926519d23ecb10
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-03-23 07:50:42 +0000

    Signed-off-by: Harshit Kumar <harshit.kumar3@gds.ey.com>

--- simpleconsoleapplinq/simpleconsoleapplinq/joinprgm.cs
+++ simpleconsoleapplinq/simpleconsoleapplinq/joinprgm.cs
@@ -26,12 +26,15 @@ namespace simpleconsoleapplinq
             IList<employee1> emp1 = new List<employee1>() 
             {
                 new employee1 () { id = 1 , dept = "Developer" , name = "Harshit1"},
-                new employee1 () { id = 2 , dept = "Testing" , name = "Harshit1"},
-                new employee1 () { id = 3 , dept = "Designing" , name = "Harshit1"},
-                new employee1 () { id = 4 , dept = "Sales" , name = "Harshit1"},
-                new employee1 () { id = 5 , dept = "Management" , name = "Harshit1"},
-                new employee1 () { id = 6 , dept = "HR" , name = "Harshit1"},
-                new employee1 () { id = 7 , dept = "Payroll" , name = "Harshit1"}
+                new employee1 () { id = 2 , dept = "Testing" , name = "Harshit2"},
+                new employee1 () { id = 3 , dept = "Designing" , name = "Harshit3"},
+                new employee1 () { id = 4 , dept = "Sales" , name = "Harshit4"},
+                new employee1 () { id = 5 , dept = "Management" , name = "Harshit5"},
+                new employee1 () { id = 6 , dept = "HR" , name = "Harshit6"},
+                new employee1 () { id = 7 , dept = "Payroll" , name = "Harshit7"},
+                new employee1 () { id = 8 , dept = "HR" , name = "Harshit8"},
+                new employee1 () { id = 9 , dept = "HR" , name = "Harshit9"},
+                new employee1 () { id = 10 , dept = "Payroll" , name = "Harshit10"}
             };
 
             IList<employee2> emp2 = new List<employee2>()
@@ -45,6 +48,7 @@ namespace simpleconsoleapplinq
                 new employee2 () { id = 7 , salary = 870000 , city = "Delhi7"}
             };
 
+            Console.WriteLine("inner join/join");
             //join using query
             var res = from e1 in emp1
                       join e2 in emp2
@@ -79,6 +83,24 @@ namespace simpleconsoleapplinq
                 Console.WriteLine(" Name : " + i.EmployeeName + " Salary : " + i.EmployeeSalary);
             }
 
+            Console.WriteLine();
+            Console.WriteLine("Left join");
+            Console.WriteLine();
+
+            var res3 = from e1 in emp1
+                       join e2 in emp2
+                       on e1.id equals e2.id into empdept
+                       from i in empdept.DefaultIfEmpty()
+                       select new
+                       {
+                           EmployeeName = e1.name,
+                           DepartmentName = i == null ? "No City" : i.city
+                       };
+
+            foreach (var i in res3) 
+            {
+                Console.WriteLine("Name : " + i.EmployeeName + " city: " + i.DepartmentName);
+            }
         }
     }
 }
