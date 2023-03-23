425866ffa1265c624ed87599e477dc0677674820
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-03-23 04:38:27 +0000

    Signed-off-by: Harshit Kumar <harshit.kumar3@gds.ey.com>

---
+++ simpleconsoleapplinq/simpleconsoleapplinq/firstordefaultprgm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simpleconsoleapplinq
+{
+    class Firstordefaultprgm
+    {
+        static void Main(string[] args)
+        {
+            int[] vals = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] vals2 = {0 };
+
+            Console.WriteLine(vals.FirstOrDefault(x => x>5));
+            Console.WriteLine(vals2.FirstOrDefault(x => x > 5));
+
+            int[] vals3 = {};
+
+            
+            var a = vals3.DefaultIfEmpty();
+            Console.WriteLine(a);
+            //Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
--- simpleconsoleapplinq/simpleconsoleapplinq/ienumprgm.cs
+++ simpleconsoleapplinq/simpleconsoleapplinq/ienumprgm.cs
@@ -1,81 +1,96 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace simpleconsoleapplinq
-{
-    class Product : IEnumerable
-    {
-        public string[] ProductList = new string[5];
-        int index = 0;
+//namespace simpleconsoleapplinq
+//{
+//    class Product : IEnumerable
+//    {
+//        public string[] ProductList = new string[5];
+//        int index = 0;
 
-        public void AddProduct(string product) 
-        {
-            ProductList[index++] = product;
-        }
+//        public void AddProduct(string product) 
+//        {
+//            ProductList[index++] = product;
+//        }
 
-        public void RemoveProduct(int index) 
-        {
-            ProductList[index] = "";
-        }
+//        public void RemoveProduct(int index) 
+//        {
+//            ProductList[index] = "";
+//        }
 
-        public IEnumerator GetEnumerator() 
-        {
-            return new ProductEnumerator();
-        }
+//        public IEnumerator GetEnumerator() 
+//        {
+//            return new ProductEnumerator();
+//        }
 
-        class ProductEnumerator : IEnumerator 
-        {
-            public object Current => throw new NotImplementedException();
+//        class ProductEnumerator : IEnumerator 
+//        {
+//            public object Current => throw new NotImplementedException();
 
-            public bool MoveNext() 
-            {
-                throw new NotImplementedException();
-            }
+//            public bool MoveNext() 
+//            {
+//                throw new NotImplementedException();
+//            }
 
-            public void Reset()
-            {
-                throw new NotImplementedException();
-            }
-        }
-    }
+//            public void Reset()
+//            {
+//                throw new NotImplementedException();
+//            }
+//        }
+//    }
 
-    class ienumprgm
-    {
-        static void Main(string[] args)
-        {
-            Product p1 = new Product();
+//    class ienumprgm
+//    {
+//        static void Main(string[] args)
+//        {
+//            Product p1 = new Product();
 
-            p1.AddProduct("a");
-            p1.AddProduct("b");
+//            p1.AddProduct("a");
+//            p1.AddProduct("b");
 
-            p1.RemoveProduct(0);
+//            p1.RemoveProduct(0);
 
-            Console.WriteLine("Products are : ");
-            foreach (var i in p1.ProductList) 
-            {
-                Console.WriteLine(i);
-            }
-
-
+//            Console.WriteLine("Products are : ");
+//            foreach (var i in p1.ProductList) 
+//            {
+//                Console.WriteLine(i);
+//            }
 
 
-            int[] arr = new int[5] { 10,20,30,40,50};
 
-            var res = arr.AsEnumerable();
 
-            foreach (var ele in res) 
-            {
-                Console.WriteLine(ele);
-            }
+//            int[] arr = new int[5] { 10,20,30,40,50};
 
-            int res1 = Queryable.Sum(arr.AsQueryable());
-            Console.WriteLine("Sum :   "+res1);
+//            var res = arr.AsEnumerable();
 
-            Console.ReadKey(); 
-        }
-    }
-}
+//            foreach (var ele in res) 
+//            {
+//                Console.WriteLine(ele);
+//            }
+
+//            int res1 = Queryable.Sum(arr.AsQueryable());
+//            Console.WriteLine("Sum :   "+res1);
+
+
+//            ArrayList obj = new ArrayList();
+//            obj.Add("aaa");
+//            obj.Add("bbb");
+//            obj.Add("ccc");
+//            obj.Add("ddd");
+//            obj.Add("eee");
+
+//            IEnumerable<string> result = obj.Cast<string>();
+
+//            foreach (var item in result) 
+//            {
+//                Console.WriteLine(item);
+//            }
+
+//            Console.ReadKey(); 
+//        }
+//    }
+//}
