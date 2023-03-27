81fb25fcf31f37c0e4cfcd5fbde2ff03f1ef626b
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-03-27 07:30:26 +0000

    Signed-off-by: Harshit Kumar <harshit.kumar3@gds.ey.com>

--- newwebservicedotnetapplication/NewConsoleCoreApp/Program.cs
+++ newwebservicedotnetapplication/NewConsoleCoreApp/Program.cs
@@ -1,18 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using newwebappservice.ServiceReference1;
+using System.ServiceModel;
 
 
-namespace ConsoleApp1
+namespace NewConsoleCoreApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-           WebService1SoapClient c1 = new WebService1SoapClient();
+            WebService1SoapClient c1 = new WebService1SoapClient();
 
             Console.WriteLine("Enter num1: ");
             int n1 = int.Parse(Console.ReadLine());
@@ -25,6 +22,7 @@ namespace ConsoleApp1
             Console.WriteLine(result);
 
             Console.ReadKey();
+
         }
     }
 }
