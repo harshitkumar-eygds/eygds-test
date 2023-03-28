3a895fde5a425f6f84be9b2dae7b2152908ca907
Harshit Kumar <harshit.kumar3@gds.ey.com>
2023-03-28 12:04:45 +0000

    Signed-off-by: Harshit Kumar <harshit.kumar3@gds.ey.com>

---
+++ ASP.netCOREWEBAPI/ASP.netCOREWEBAPI/Models/appdbcontext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.netCOREWEBAPI.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options) { }
+
+        public DbSet<Department> Departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
